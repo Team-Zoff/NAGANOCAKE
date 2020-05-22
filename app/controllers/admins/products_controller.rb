7784fa75de83fc94af8da8a92a80c73f9cba8161
rina-1 <cba.mac@icloud.com>
2020-05-22 07:45:33 +0000

    ri-chan

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -9,7 +9,8 @@ class Admins::ProductsController < ApplicationController
 		product = Product.new(product_params)
 		product.save
 	end
-
+	def index
+	end
 	    private
     def product_params
         params.require(:product).permit(:product_image_id,:product_name,:description,:genre_id,:price_excluding_tax,:sale_status)
