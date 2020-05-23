a9c6f70ebd4e3fd3da870e12d6472ecc3d451ce0
rina-1 <cba.mac@icloud.com>
2020-05-23 10:28:12 +0000

    コントローラー

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,21 +1,29 @@
 class Admins::OrdersController < ApplicationController
-	class Admins::OrdersController < ApplicationController
 	def index
 		if params[:page_id] == 1
 			@orders = Order.all
 		elsif params[:page_id] == 2
 			@orders = Order.where('created_at > ?', 1.day)
 		else
-			@orders = Order.all
+			@orders = Order.find(params[:id])
 			#該当顧客のデータ　あとで作ります
 		end
 
-		# link_to hogehoge_path(page_id: 1)
-		# link_to hogehoge_path, page_id: 1
+
 	end
 	def show
-		# @order = Order.find(current_member)
-		# @order_products = Order_detail.order_id
+		# @order_products = @orderer.order_details
+		# @items = Order_detail.new(@order_products.each)
+
+		@order = Order.find(params[:id])
+		@order_details = @order.order_details
+		# @products = []
+		# @order_details.each do |order_detail|
+		# 	@products.push(order_detail.product)
+		# end
+		# @products.each do |product|
+		# 	product.name
+		# end
+
 	end
-end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,6 @@ class Order < ApplicationRecord
 	has_many :order_details, dependent: :destroy
 	belongs_to :member
 
-	enum order_status: {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
+	enum method_of_payment: {"銀行決済": 0, "クレジットカード決済": 1}
+	enum order_status: {"入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
 end
