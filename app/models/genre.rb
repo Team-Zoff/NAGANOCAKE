562681d66e3cc2ba2c62abc77f4f17e2abbf4043
yudai-suzuki-7 <el.pocho.boca@gmail.com>
2020-05-29 02:56:54 +0000

    完成(仮)

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,8 +3,8 @@ class Genre < ApplicationRecord
 
   attachment :genre_image
 
-
   validates :name, presence: true
   validates :genre_image, presence: true
   enum valid_invalid:{Validity: 0, Invalid: 1}
+
 end
